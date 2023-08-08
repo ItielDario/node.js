d91064b79efea2007fb2219ceec633078f77ac1f
Itiel Dario <itiel.dario@gmail.com>
2023-08-08 17:53:48 -0300

    Entendendo sobre Global Objects

---
+++ Ex 03 - Global Object/global.js
@@ -0,0 +1,12 @@
+// console.log('OBJETO GLOBAL');
+// console.log(global);
+
+console.clear();
+
+//console.log('Teste')
+// setTimeout(() => {
+//     console.log('Terminado');
+// }, 3000);
+
+console.log(__dirname); // MOSTRA O DIRETÓRIO ATUAL NA QUAL ESTE ARQUIVO ESTÁ SENDO EXECULTADO 'Ex 03 - Global Object'
+console.log(__filename); // EXIBE O CAMINHO COMPLETO DO ARQUIVO ATUAL 'global.js'
