53f2109ec07465689b93a07bdff7745fc4acf541
Itiel Dario <itiel.dario@gmail.com>
2023-08-14 20:11:49 -0300

    Exibindo páginas html

--- Ex 14 - Express/app.js
+++ Ex 14 - Express/app.js
@@ -10,11 +10,27 @@ const app = express()  // CRIA UM OBJETO APARTIR DO EXPRESS
 app.listen(3000);
 
 // CRIANDO AS ROTAS
-app.get('/', (req, res) => {  // QUANDO SOLICITADO A PÁGINA RAIZ '/' EXECULTA A FUNÇÃO  
-    
+app.get('/', (req, res) => {  // QUANDO CHAMADA A RAIZ '/' EXECULTA A FUNÇÃO  
     res.send('<h1>Aplicação com express</h1>')  // ENVIA UMA REPOSTA
-})
+});
 
-app.get('/about', (req, res) => {
+app.get('/about', (req, res) => {  // QUANDO CHAMADA A URL '/ABOUT' EXECULTA A FUNÇÃO  
     res.send('<h1>Sobre nós</h1>');
+});
+
+
+// EXIBINDO UMA PÁGINA HTML
+// HÁ DUAS FORMAS:
+
+// 1º FORMA -> INDICAR O CAMINHO ABSOLUTO DO ARQUIVO
+app.get('/contact', (req, res) => {
+    res.sendFile('C:/Users/itiel/OneDrive/Documentos/Estudos/Estudos/node.js/Ex 14 - Express/views/contact.html'); // CAMINHO ABSOLUTO
+});
+
+// 2º FORMA -> ESPECIFICAR A RAIZ E DEFINIR AS SUAS RELAÇÕES
+app.get('/info', (req, res) => {
+
+    // SELECIONA A PASTA ONDE ESTA SENDO EXECULTADA O APP.JS (__DIRNAME) E DEFINE ESTA" COMO A RAIZ.
+    // E ESPECIFICA O CAMINHO DO ARQUIVO (./VIEWS/INFO.HTML) COMO EXTENÇÃO DA RAIZ -> "__DIRNAME/VIEWS/INFO.HTML"
+    res.sendFile('./views/info.html', { root: __dirname });  
 })
