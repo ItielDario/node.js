4522e29a68b8d5f1b77f25950604e115d3ef95e1
Itiel Dario <itiel.dario@gmail.com>
2023-08-19 14:33:45 -0300

    Entendendo o morgan

--- Ex 16 - Middleware/app.js
+++ Ex 16 - Middleware/app.js
@@ -1,12 +1,15 @@
-// MIDDLEWARE -> SÃO CÓDIGOS QUE SÃO EXECULTADOS ENTRE A REQUISIÇÃO E A RESPOSTA
+// MIDDLEWARE -> SÃO CÓDIGOS QUE SÃO EXECULTADOS ENTRE A REQUISIÇÃO E A RESPOSTA 
 /*
     - REGISTRO DE LOGS 
     - FUNCIONALIDADES DE AUTENTICAÇÃO E AUTORIZAÇÃO
     - VALIDAÇÃO E PREPARAÇÃO DE DADOS JSON A PARTIR DO REQUEST
     - RESPONDER ERROS DA APLICAÇÃO
 */ 
+// MORGAN -> É UM MIDDLEWARE QUE REGISTRA INFORMÇÕES DE SOLICITAÇÕES HTTP
+// PARA BAIXAR -> npm install morgan
 
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express()  
 
@@ -15,17 +18,26 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 
+
 // MIDDLEWARE ------------------------------------------------------------------------------------------------------------------
+// FORMA TRADICIONAL
 app.use((req, res, next) => {  // COMO NÃO ESPECIFICA A ROTA, SEMPRE QUE SOLICITADO 'LOCALHOST:3000' A FUNÇÃO SERÁ EXECULTADA 
 
     console.log('Novo pedio');
     console.log('Host: ' + req.hostname);
     console.log('Path: ' + req.path);
-    console.log('Method: ' + req.method);
+    console.log('Method: ' + req.method + '\n\n');
 
     // TEMOS QUE DEFINIR O QUE FAZER DEPOIS POIS O SERVIDOR FICA AGUANDANDO UMA RESPOSTA A SER ENTREGUE
     next();  // INDICA AO SERVIDOR QUE DEPOIS DE EXECULTAR A FUNÇÃO PODERÁ CONTINUAR EXECULTANDO O CÓDIGO ABAIXO
 });
+
+
+// UTILIZANDO O MORGAN (MONITORA O COMPORTAMENTO DO SISTEMA)
+app.use(morgan('tiny')); // EXIBE INFORMAÇÕES BÁSICAS SOBRE A REQUISIÇÃO (UTILIZA O MORGAN NO FORMATO 'TINY')
+app.use(morgan('dev')); // O FORMATO DEV EXIBE INFORMAÇÕES NO SISTEMA COM MAIS DETALHES 
+app.use(morgan(' :status :res[content-length] - :response-time ms :method :url')); // DEFINE QUAIS E COMO AS INFORMAÇÕES SERÃO EXIBIDAS 
+
 
 
 // CRIANDO AS ROTAS ------------------------------------------------------------------------------------------------------------
@@ -37,7 +49,7 @@ app.get('/', (req, res) =>{
 app.use((req, res, next) => { 
     
     // ESSA MENSAGEM SO SERÁ EXIBIDA PARA AS ROTAS ABAIXO DESTE 'USE'
-    console.log('Segundo Middleware!');
+    console.log('\n\nSegundo Middleware!\n\n');
     next();
 })
 
