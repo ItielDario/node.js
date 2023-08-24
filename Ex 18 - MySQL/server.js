9fd07f9e3b5fbc6ac8b1186892c48695478fc3c5
Itiel Dario <itiel.dario@gmail.com>
2023-08-24 18:10:36 -0300

    HTML com MySQL

--- Ex 18 - MySQL/server.js
+++ Ex 18 - MySQL/server.js
@@ -11,20 +11,20 @@ app.listen(3000, () => {
 // MYSQL
 const mysql = require('mysql');  // IMPORTA O MÓDULO SQL
 const conexao = mysql.createConnection({  // CRIA CONEXÃO COM O BANCO DE DADOS
-    host: 'localhost',  // O ENDEREÇO DO SERVIDOR DO BANCO DE DADOS
-    user: 'root',  // NOME DO USUÁRIO PARA A AUTENTICAÇÃO
-    password: '',  // SENHA DO USUÁRIO PARA AUTENTICAÇÃO
-    database: 'blogs'  // NOME DO BANCO DE DADOS QUE DESEJA CONECTAR
+    host: 'localhost',  // LOCAL ONDE ESTA O SERVIDOR MYSQL
+    user: 'user_blog',  // NOME DO USUÁRIO PARA A AUTENTICAÇÃO
+    password: 'L7sAV2PiKopIborAM58ege62ri7Ipi',
+    database: 'blog',  // NOME DA BASE DE DADOS QUE IRÁ CONSUMIR
 });
 
 
 // ROUTES
 app.get('/', (req, res) => {
     
+    // ESTABELECENDO CONEXÃO COM O SERVIDOR MYSQL
     // SELECIONA TODOS OS DADOS DA TEBELA 'users'
     conexao.query("SELECT * FROM users", (erro, resultado) => { // EXECULTA CONSULTAS SQL NO BANCO DE DADOS (PERMITE ESCREVE UM SQL LITERAL E DEPOIS EXECULTA UMA FUNÇÃO)  
 
-        
         if(erro){  // SE DER ALGUM ERRO -> 
             console.log('Deu ruim! -> ', erro.message);
             console.log('Deu ruim! -> ', erro.code);
@@ -33,6 +33,19 @@ app.get('/', (req, res) => {
             res.send('Deu ruim! -> ', erro);
         }
 
-        res.send(resultado[2].username);
+        if(resultado.length != 0){
+            
+            let html = '<ul>';
+            resultado.forEach(elem => {
+                html += `<li> ${elem.username} </li>`;
+            });
+            html += '</ul>';
+
+            res.send(html);
+        }
+        else{
+            res.send('Não existem resultados');
+        }
+        
     });  
 });
