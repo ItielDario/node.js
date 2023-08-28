dc530f824efa90910f74ed779d1fb691ccd35c8f
Itiel Dario <itiel.dario@gmail.com>
2023-08-28 19:52:05 -0300

    Validando o campo tarefas

--- Mini-Projetos/To_Do_List_1.0/server.js
+++ Mini-Projetos/To_Do_List_1.0/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const tarefaController = require('./controller/tarefaController');
+const { urlencoded } = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 app.listen(3000, () => {
     console.log('Server running');
@@ -6,5 +9,8 @@ app.listen(3000, () => {
 
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded());
 app.use(express.static('public'));
 
+const tarefa = new tarefaController();
+app.use('/', tarefa.router)
